677be45f86074a30108494946f0d2a4e9638d8e0
avery <avery@averys-air-2.wireless-1x.unc.edu>
2019-08-28 17:37:32 -0400

    Test Novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,31 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		// number of customers (first integer)
+		int numCustomers = scan.nextInt();
+		
+		for (int i = 0; i < numCustomers; i++) {
+			String first = scan.next();
+			// System.out.println(first.charAt(0));
+			String last = scan.next();
+			// System.out.println(last);
+			int numItems = scan.nextInt();
+			// System.out.println(numItems);
+			double customerSum = 0;
+			for (int j = 0; j < numItems; j++) {
+				int numProduct = scan.nextInt();
+				String productName = scan.next();
+				double productPrice = scan.nextDouble();
+				customerSum += (numProduct * productPrice);
+			// System.out.println(String.format("%.2f", customerSum));
+			}
+			String custSumString = String.format("%.2f", customerSum);
+			System.out.println(first.charAt(0) + ". " + last + ": " + custSumString);
+			
+			
+		}
+		
+		scan.close();
+		
 	}
 }
