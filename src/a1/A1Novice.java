8aff721f12db2b46bffc7a178a229af358499b6c
avery <avery@averys-air-2.wireless-1x.unc.edu>
2019-08-28 17:54:31 -0400

    updated comments

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -13,26 +13,38 @@ public class A1Novice {
 		// number of customers (first integer)
 		int numCustomers = scan.nextInt();
 		
+		// loop through all customers
 		for (int i = 0; i < numCustomers; i++) {
+			// first collect customer name (formatting in print statement)
 			String first = scan.next();
-			// System.out.println(first.charAt(0));
 			String last = scan.next();
-			// System.out.println(last);
+
+			// next input should be number of products for for that customer
 			int numItems = scan.nextInt();
-			// System.out.println(numItems);
+
+			// declare customer sum outside for loop so it is accessible for print statement
 			double customerSum = 0;
+			
+			// loop through customer products using the numItem that was scanned previously
 			for (int j = 0; j < numItems; j++) {
+				// first number is an integer that represents product quantity
 				int numProduct = scan.nextInt();
+				// scan product name so the scanner is up to date with the input
 				String productName = scan.next();
+				// second number is price of the product
 				double productPrice = scan.nextDouble();
+				// add the total cost of that product to the customer's total and continue loop
 				customerSum += (numProduct * productPrice);
 			}
+			// convert customer sum to string formatting in order to print
 			String custSumString = String.format("%.2f", customerSum);
+			
+			// combine and format elements to print
 			System.out.println(first.charAt(0) + ". " + last + ": " + custSumString);
 			
 			
 		}
-		
+		// all input has been processed to scanner can close
 		scan.close();
 		
 	}
